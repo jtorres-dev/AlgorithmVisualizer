cca1812922d56da70d79653f76f1ca37dbef510d
Jonathan Torres <jtorres_1@outlook.com>
2020-12-03 21:30:37 -0600

    Improved code readability

--- src/pathfinding/AStar.java
+++ src/pathfinding/AStar.java
@@ -58,14 +58,13 @@ public class AStar {
 
 	private void setWallNodes(int[][] coords) {
 		for(int[] coord : coords)
-			grid[coord[0]][coord[1]].setWall();
+			grid[coord[0]][coord[1]].setWall(true);
 	}
 
 	// Manhattan format
 	private void calculateHeuristics(Node end) {
 		for(int i = 1; i < grid.length; i++) {
 			for(int j = 1; j < grid[0].length; j++) {
-
 				if(grid[i][j].isWall())
 					continue;
 
@@ -85,249 +84,222 @@ public class AStar {
 					dy = j - end.getY();
 				
 				grid[i][j].setHeuristic(dx + dy);
-//				System.out.print(grid[i][j] + "-> h:" + grid[i][j].heuristic() + ", ");
 			}
-//			System.out.println();
 		}
 	}
 
-	/* 
-		I'll assume there is a more elegant way of doing this instead of hand declaring all the edge cases.
+	/*
+		I'll assume there is a more elegant way of doing this
 		TODO:
 		 - Improvements: maybe a double for loop? recursion? ...	
 	*/
 	private Node findBestNeighbor(Node curr) {
-		int rLen = grid.length;
-		int cLen = grid[0].length;
+		int width = grid.length;
+		int height = grid[0].length;
 
 		closed.add(curr);
 
-		// center
-		if(curr.getX() > 1 && curr.getX() < rLen - 1 &&
-		   curr.getY() > 1 && curr.getY() < cLen - 1) {
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY()], false); // right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY()];
+		// center node
+		if(curr.getX() > 1 && curr.getX() < width - 1 &&
+		   curr.getY() > 1 && curr.getY() < height - 1) {
 
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY()], false); // left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY()];
+			Node right = grid[curr.getX() + 1][curr.getY()];
+			Node down  = grid[curr.getX()][curr.getY() + 1];
+			Node left  = grid[curr.getX() - 1][curr.getY()];
+			Node up    = grid[curr.getX()][curr.getY() - 1];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() + 1], false); // down
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() + 1];
+			Node upperRight  = grid[curr.getX() + 1][curr.getY() - 1];
+			Node bottomRight = grid[curr.getX() + 1][curr.getY() + 1];
+			Node bottomLeft  = grid[curr.getX() - 1][curr.getY() + 1];
+			Node upperLeft   = grid[curr.getX() - 1][curr.getY() - 1];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() - 1], false); // up
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() - 1];
+			upperRight.setDiagonal(true);
+			bottomRight.setDiagonal(true);
+			bottomLeft.setDiagonal(true);
+			upperLeft.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() + 1], true); // bot right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() + 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, right, down, left, up, upperRight, bottomRight, bottomLeft, upperLeft);
 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() - 1], true); // upper right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() - 1];
-
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() + 1], true); // bot left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() + 1];
-
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() - 1], true); // upper left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() - 1];
-		} 
-
-		// left edge
-		else if(curr.getX() == 1 && curr.getY() > 1 && curr.getY() < cLen - 1) {
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY()], false); // right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY()];
-
-			addNeighbor(curr, grid[curr.getX()][curr.getY() + 1], false); // down
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() + 1];
-
-			addNeighbor(curr, grid[curr.getX()][curr.getY() - 1], false); // up
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() - 1];
-
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() + 1], true); // bot right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() + 1];
-
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() - 1], true); // upper right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() - 1];
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// top edge
-		else if(curr.getY() == 1 && curr.getX() > 1 && curr.getX() < rLen - 1) { 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY()], false); // right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY()];
+		// left edge node
+		else if(curr.getX() == 1 && curr.getY() > 1 && curr.getY() < height - 1) {
+			Node right = grid[curr.getX() + 1][curr.getY()];
+			Node down  = grid[curr.getX()][curr.getY() + 1];
+			Node up    = grid[curr.getX()][curr.getY() - 1];
 
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY()], false); // left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY()];
+			Node upperRight  = grid[curr.getX() + 1][curr.getY() - 1];
+			Node bottomRight = grid[curr.getX() + 1][curr.getY() + 1];
+			upperRight.setDiagonal(true);
+			bottomRight.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() + 1], false); // down
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() + 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, right, down, up, upperRight, bottomRight);
 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() + 1], true); // bot right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() + 1];
-
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() + 1], true); // bot left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() + 1];
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// right edge
-		else if(curr.getX() == rLen - 1 && curr.getY() > 1 && curr.getY() < cLen - 1) {
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY()], false); // left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY()];
+		// top edge node
+		else if(curr.getY() == 1 && curr.getX() > 1 && curr.getX() < width - 1) {
+			Node right = grid[curr.getX() + 1][curr.getY()];
+			Node down  = grid[curr.getX()][curr.getY() + 1];
+			Node left  = grid[curr.getX() - 1][curr.getY()];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() + 1], false); // down
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() + 1];
+			Node bottomRight = grid[curr.getX() + 1][curr.getY() + 1];
+			Node bottomLeft  = grid[curr.getX() - 1][curr.getY() + 1];
+			bottomRight.setDiagonal(true);
+			bottomLeft.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() - 1], false); // up
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() - 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, right, down, left, bottomRight, bottomLeft);
 
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() + 1], true); // bot left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() + 1];
-
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() - 1], true); // upper left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() - 1];
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// bottom edge
-		else if(curr.getY() == cLen - 1 && curr.getX() > 1 && curr.getX() < rLen - 1) {
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY()], false); // right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY()];
+		// right edge node
+		else if(curr.getX() == width - 1 && curr.getY() > 1 && curr.getY() < height - 1) {
+			Node down  = grid[curr.getX()][curr.getY() + 1];
+			Node left  = grid[curr.getX() - 1][curr.getY()];
+			Node up    = grid[curr.getX()][curr.getY() - 1];
 
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY()], false); // left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY()];
+			Node bottomLeft  = grid[curr.getX() - 1][curr.getY() + 1];
+			Node upperLeft   = grid[curr.getX() - 1][curr.getY() - 1];
+			bottomLeft.setDiagonal(true);
+			upperLeft.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() - 1], false); // up
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() - 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, down, left, up, bottomLeft, upperLeft);
 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() - 1], true); // upper right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() - 1];
-
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() - 1], true); // upper left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() - 1];
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// top left corner
+		// bottom edge node
+		else if(curr.getY() == height - 1 && curr.getX() > 1 && curr.getX() < width - 1) {
+			Node right = grid[curr.getX() + 1][curr.getY()];
+			Node left  = grid[curr.getX() - 1][curr.getY()];
+			Node up    = grid[curr.getX()][curr.getY() - 1];
+
+			Node upperRight  = grid[curr.getX() + 1][curr.getY() - 1];
+			Node upperLeft   = grid[curr.getX() - 1][curr.getY() - 1];
+			upperRight.setDiagonal(true);
+			upperLeft.setDiagonal(true);
+
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, right, left, up, upperRight, upperLeft);
+
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
+		}
+
+		// top left corner node
 		else if(curr.getX() == 1 && curr.getY() == 1) {
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY()], false); // right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY()];
+			Node right = grid[curr.getX() + 1][curr.getY()];
+			Node down  = grid[curr.getX()][curr.getY() + 1];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() + 1], false); // down
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() + 1];
+			Node bottomRight = grid[curr.getX() + 1][curr.getY() + 1];
+			bottomRight.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() + 1], true); // bot right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() + 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, right, down, bottomRight);
+
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// top right corner
-		else if(curr.getX() == rLen - 1 && curr.getY() == 1) {
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY()], false); // left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY()];
+		// top right corner node
+		else if(curr.getX() == width - 1 && curr.getY() == 1) {
+			Node down = grid[curr.getX()][curr.getY() + 1];
+			Node left = grid[curr.getX() - 1][curr.getY()];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() + 1], false); // down
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() + 1];
+			Node bottomLeft = grid[curr.getX() - 1][curr.getY() + 1];
+			bottomLeft.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() + 1], true); // bot left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() + 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() + 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, down, left, bottomLeft);
+
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// bottom right corner
-		else if(curr.getX() == rLen - 1 && curr.getY() == cLen - 1) {
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY()], false); // left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY()];
+		// bottom right corner node
+		else if(curr.getX() == width - 1 && curr.getY() == height - 1) {
+			Node left = grid[curr.getX() - 1][curr.getY()];
+			Node up   = grid[curr.getX()][curr.getY() - 1];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() - 1], false); // up
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() - 1];
+			Node upperLeft = grid[curr.getX() - 1][curr.getY() - 1];
+			upperLeft.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX() - 1][curr.getY() - 1], true);  // upper left
-			// check if neighbor is the end node
-			if(grid[curr.getX() - 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() - 1][curr.getY() - 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, left, up, upperLeft);
+
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
-		// bottom left corner
-		else if(curr.getX() ==  1 && curr.getY() == cLen - 1) {
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY()], false); // right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY()].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY()];
+		// bottom left corner node
+		else if(curr.getX() ==  1 && curr.getY() == height - 1) {
+			Node right = grid[curr.getX() + 1][curr.getY()];
+			Node up    = grid[curr.getX()][curr.getY() - 1];
 
-			addNeighbor(curr, grid[curr.getX()][curr.getY() - 1], false); // up
-			// check if neighbor is the end node
-			if(grid[curr.getX()][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX()][curr.getY() - 1];
+			Node upperRight  = grid[curr.getX() + 1][curr.getY() - 1];
+			upperRight.setDiagonal(true);
 
-			addNeighbor(curr, grid[curr.getX() + 1][curr.getY() - 1], true); // upper right
-			// check if neighbor is the end node
-			if(grid[curr.getX() + 1][curr.getY() - 1].sameLocation(end))
-				return grid[curr.getX() + 1][curr.getY() - 1];
+			List<Node> neighbors = new ArrayList<>();
+			Collections.addAll(neighbors, right, up, upperRight);
+
+			for(Node neighbor : neighbors) {
+				if(neighbor.sameLocation(end)) {
+					neighbor.setPrevious(curr);
+					return neighbor;
+				}
+				addNeighbor(curr, neighbor);
+			}
 		}
 
 		/* sorts by Node's f() value */
@@ -337,16 +309,11 @@ public class AStar {
 		return open.remove(0);
 	}
 
-	private void addNeighbor(Node curr, Node neighbor, boolean diagonal) {
+	private void addNeighbor(Node curr, Node neighbor) {
 		if(neighbor.isWall() || closed.contains(neighbor))
 			return;
 
-		if(neighbor.sameLocation(end)) {
-			neighbor.setPrevious(curr);
-			return;
-		}
-
-		if(diagonal) {
+		if(neighbor.isDiagonal()) {
 			// if curr is better option. (Relax edge)
 			if(open.contains(neighbor) && neighbor.cost() > curr.cost() + DIAGONAL_COST)
 				neighbor.setPrevious(curr);
--- src/pathfinding/Node.java
+++ src/pathfinding/Node.java
@@ -4,6 +4,7 @@ public class Node {
 
 	private int x, y, cost, heuristic;
 	private boolean isWall;
+	private boolean isDiagonal;
 	private Node prev;
 
 	public Node(int x, int y) {
@@ -11,6 +12,7 @@ public class Node {
 		this.y = y;
 		cost = 0;
 		isWall = false;
+		isDiagonal = false;
 		prev = null;
 	}
 
@@ -21,23 +23,25 @@ public class Node {
 	int cost() { return cost; }
 
 	int heuristic() { return heuristic; }
-	
+
 	int f() { return cost + heuristic; }
 
 	Node previous() { return prev; }
 
 	boolean isWall() { return isWall; }
 
+	boolean isDiagonal() { return isDiagonal; }
 
 
 	void setHeuristic(int heuristic) { this.heuristic = heuristic; }
 
 	void setCost(int cost) { this.cost = cost; }
-	
-	void setPrevious(Node prev) { this.prev = prev; }
-	
-	void setWall() { isWall = true; }
 
+	void setPrevious(Node prev) { this.prev = prev; }
+
+	void setWall(boolean isWall) { this.isWall = isWall; }
+
+	void setDiagonal(boolean isDiagonal) { this.isDiagonal = isDiagonal; }
 
 
 	boolean sameLocation(Node other) { return getX() == other.getX() && getY() == other.getY(); }
